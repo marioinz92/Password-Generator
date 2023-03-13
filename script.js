a535a77297404cb8f316159190e429189aac2b64
Mario Inzunza <marioelgallo92@gmail.com>
2023-03-12 18:21:52 -0700

    commit

--- script.js
+++ script.js
@@ -11,4 +11,45 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var password = "";
+  
+  // WHEN prompted for password criteria
+  // THEN I select which criteria to include in the password
+  
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters 
+  const passwordLength = prompt ("How long would you like your password?")
+  
+
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  var lowerCase = confirm("Do you want lowercase characters?");
+  var upperCase = confirm("Do you want uppercase characters?");
+  var numeric = confirm("Do you want numeric characters?");
+  var special = confirm("Do you want special characters?");
+  
+  
+  
+  
+  
+  // WHEN I answer each prompt
+  // THEN my input should be validated and at least one character type should be selected
+  
+
+  // WHEN all prompts are answered
+  // THEN a password is generated that matches the selected criteria
+  
+
+  // WHEN the password is generated
+  // THEN the password is either displayed in an alert or written to the page
+  
+  return password;
+}
+
+
+
